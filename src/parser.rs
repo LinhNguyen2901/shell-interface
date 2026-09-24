b81b118983264e9408759dae7b988683aad6d6e3
hoangdung-nguyen <nguyenhoangdung21006@gmail.com>
2026-09-24 17:20:46 -0400

    Piping

--- src/parser.rs
+++ src/parser.rs
@@ -1,17 +1,30 @@
+use nix::{sys::wait::waitpid,unistd::{fork, ForkResult, write, close, execvp, dup2, pipe, Pid}};
+use std::os::fd::{AsRawFd, RawFd, OwnedFd};
+use std::ffi::CString;
+
 // Adapted from https://www.cs.purdue.edu/homes/grr/SystemsProgrammingBook/Book/Chapter5-WritingYourOwnShell.pdf
 #[derive(Debug)]
 pub struct SimpleCommand {
     pub arguments: Vec<String>,
+    pub out_file: Option<String>,
+    pub in_file: Option<String>,
+    pub err_file: Option<String>,
 }
 impl SimpleCommand {
     pub fn new() -> Self {
         Self {
             arguments: Vec::new(),
+            out_file: None,
+            in_file: None,
+            err_file: None,
         }
     }
     pub fn new_with_command(cmd: &str) -> Self {
         Self {
             arguments: vec![cmd.to_string()],
+            out_file: None,
+            in_file: None,
+            err_file: None,
         }
     }
     pub fn insert_argument(&mut self, argument: String) {
@@ -22,9 +35,6 @@ impl SimpleCommand {
 #[derive(Debug)]
 pub struct Command {
     pub simple_commands: Vec<SimpleCommand>,
-    pub out_file: Option<String>,
-    pub in_file: Option<String>,
-    pub err_file: Option<String>,
     pub background: bool,
 }
 
@@ -32,9 +42,6 @@ impl Command {
     pub fn new() -> Self {
         Self {
             simple_commands: Vec::new(),
-            out_file: None,
-            in_file: None,
-            err_file: None,
             background: false,
         }
     }
@@ -42,30 +49,65 @@ impl Command {
         // TODO: checking
         self.simple_commands.push(simple_command);
     }
-    pub fn prompt(&self) {
-        
-    }
-    pub fn print(&self) {
-        
-    }
     pub fn execute(&self) {
-        // Temporary, only echo
-        for cmd in &self.simple_commands {
-            if cmd.arguments.first().map(|s| s.as_str()) == Some("echo") {
-                if let Some(arg) = cmd.arguments.get(1) {
-                    println!("{}", arg);
+        if self.simple_commands.is_empty() { return; }
+        let mut inputfd: RawFd = 0;
+        let mut children: Vec<Pid> = Vec::new();
+        let len = self.simple_commands.len();
+
+        for (i, cmd) in self.simple_commands.iter().enumerate() {
+
+            let last = i == len - 1;
+            let mut pipefd: (RawFd, RawFd) = (-1, -1);
+            if !last {
+                let (i, o) = pipe().expect("Failed to create pipe.");
+                pipefd = (i.as_raw_fd(), o.as_raw_fd())
+            }
+            match unsafe{fork()} {
+                Ok(ForkResult::Parent { child, .. }) => {
+                    children.push(child);
+                    if inputfd != 0 {
+                        close(inputfd).ok();
+                    }
+                    if !last {
+                        close(pipefd.1).ok();
+                        
+                    }
                 }
+                Ok(ForkResult::Child) => {
+                    write(std::io::stdout(), "I'm a new child process\n".as_bytes()).ok();
+                    if inputfd != 0 {
+                        dup2(inputfd, 0).expect("Failed to redirect stdin");
+                        close(inputfd).ok();
+                    }
+                    if !last {
+                        close(pipefd.0).ok();
+                        dup2(pipefd.1, 1).expect("Failed to redirect stdout");
+                        close(pipefd.1).ok();
+                    }
+                    let program_cstr = CString::new(cmd.arguments[0].as_str()).unwrap();
+                    let args_cstrs: Vec<CString> = cmd.arguments.iter().map(|s| CString::new(s.as_str()).unwrap()).collect();
+                    execvp(&program_cstr, &args_cstrs).expect("Exec failed");
+                    unsafe { libc::_exit(0) };
+                }
+                Err(_) => println!("Fork failed"),
             }
-            else {
-                println!("Command {:?} not found.", cmd);
+            for pid in children {
+                let _ = waitpid(pid, None);
             }
+
+            // if cmd.arguments.first().map(|s| s.as_str()) == Some("echo") {
+            //     if let Some(arg) = cmd.arguments.get(1) {
+            //         println!("{}", arg);
+            //     }
+            // }
+            // else {
+            //     println!("Command {:?} not found.", cmd);
+            // }
         }
     }
     pub fn clear(&mut self) {
         self.simple_commands.clear();
-        self.out_file = None;
-        self.in_file = None;
-        self.err_file = None;
         self.background = false;
     }
 }
@@ -103,8 +145,14 @@ pub fn parse_tokens(tokens: Vec<&str>) -> Command {
                     last_token = Vocab::WORD;
                 },
                 Vocab::PIPE => {output.insert_simple_command(SimpleCommand::new_with_command(tok)); last_token = Vocab::CMD;},
-                Vocab::GREAT => {output.out_file = Some(tok.to_string()); last_token = Vocab::NONE;}
-                Vocab::LESS => {output.in_file = Some(tok.to_string()); last_token = Vocab::NONE;}
+                Vocab::GREAT => {if let Some(last_cmd) = output.simple_commands.last_mut() {
+                        last_cmd.out_file = Some(tok.to_string());
+                    } 
+                    last_token = Vocab::NONE;}
+                Vocab::LESS => {if let Some(last_cmd) = output.simple_commands.last_mut() {
+                        last_cmd.in_file = Some(tok.to_string());
+                    } 
+                    last_token = Vocab::NONE;}
                 Vocab::NONE => println!("Error")
             }
         }
